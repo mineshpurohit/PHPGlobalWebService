095d4c5e45ec33553bd66c2ad457589e545b7def
Minesh Purohit <minesh.purohit1@googlemail.com>
2014-11-17 17:27:44 +0530

    Create serverInfo.php

---
+++ serverInfo.php
@@ -0,0 +1,8 @@
+<?php
+
+$host = 'localhost';  // Host Address, Ex: localhost
+$user = 'admin';  // Database User Name, Ex: admin 
+$pass = 'admin';  // Database Password, Ex: admin
+$db = 'dbName';    // Database Name, Ex: testDb
+
+?>
